90fd1c11f9c7f1c65dfd9ff165853337504faa69
Taylor Bird <nsubordin81@gmail.com>
2025-03-14 16:05:12 -0400

    working through the examples for this

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ import streamlit as st
 def main():
     st.header("welcome to the streamlit cli!")
 
-
+    components = ["openai_connector", "document_parser"]
 
     st.multiselect(
         label="select a component",
-        options=[1, 2, 3],
+        options=components,
+        key="multi_select",
     )    
 
     st.button("build project", on_click=build_project)
 
 
 def build_project():
-    st.write("I glot clicked")
+    for component in st.session_state.multi_select:
+        st.write (f" adding {component} to the project")
+    st.write(st.session_state.multi_select[0])
 
 
 
